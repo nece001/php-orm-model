da06d698ae02264a577c804c486bc37506da5f58
nece001 <nece001@163.com>
2023-05-23 21:38:53 +0800

    '整理代码'

--- src/TP5Model.php
+++ src/TP5Model.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace Nece001\PhpOrmModel\Interfaces;
+namespace Nece001\PhpOrmModel;
 
+use Nece001\PhpOrmModel\Interfaces\Model as InterfacesModel;
 use think\Model;
 
-class TP5Model extends Model implements Model
+class TP5Model extends Model implements InterfacesModel
 {
 
     /**
@@ -54,5 +55,20 @@ class TP5Model extends Model implements Model
      */
     public function rollback()
     {
+    }
+
+    /**
+     * 设置表别名
+     *
+     * @Author gjw
+     * @DateTime 2023-05-23
+     *
+     * @param string $alias
+     *
+     * @return $this
+     */
+    public function alias($alias)
+    {
+        return parent::alias($alias);
     }
 }
--- src/TP6Model.php
+++ src/TP6Model.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace Nece001\PhpOrmModel\Interfaces;
+namespace Nece001\PhpOrmModel;
 
+use Nece001\PhpOrmModel\Interfaces\Model as InterfacesModel;
 use think\Model;
 
-class TP6Model extends Model implements Model
+class TP6Model extends Model implements InterfacesModel
 {
     /**
      * 开启事务
@@ -43,5 +44,20 @@ class TP6Model extends Model implements Model
     public function rollback()
     {
         $this->db()->rollback();
+    }
+
+    /**
+     * 设置表别名
+     *
+     * @Author gjw
+     * @DateTime 2023-05-23
+     *
+     * @param string $alias
+     *
+     * @return $this
+     */
+    public function alias($alias)
+    {
+        return parent::alias($alias);
     }
 }
